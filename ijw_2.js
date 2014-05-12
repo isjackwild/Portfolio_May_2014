bbdc5850eea1a06b7da764bcf98fd7aaff038c1e
Jack Wild <jack.wild@live.co.uk>
2014-05-12 18:20:39 +0200

    initial commit

---
+++ ijw_2.js
@@ -0,0 +1,110 @@
+var leftQuote = ["How did a girl like you", "order order order", "Ignor", "I buy junk", "Lost dog, recently castrated", "The Ark: Designed by an amature", "Left.", "The Devil is in the", "I like my men like I like my tea", "", "Lonely.", "No mistakes", "All views expressed are", "My other car is"]
+var rightQuote = ["end up in a place like this?", "DISORDER.", "(ed).", "and sell antiques.", "answers to name of 'Lucky'.", "The Titanic: Designed by an expert.", "Wrong.", "deta>:->ls.", "Ginger.", "Lost.", "", "undefined", "someone else's.", "the U7 to Südstern."]
+
+var buzzWords = ["Photoshop", "Illustrator", "inDesign", "AfterEffects", "Final Cut Pro", "Processing", "Arduino", "HTML", "HTML Canvas", "CSS", "CSS Animation", "Spritesheet Animation", "Javascript", "CoffeeScript", "pen(cil) and paper", "paper prototyping", "design in code"]
+
+var colours = ["orange", "red", "blue", "teal"]
+
+var lastQuote;
+var whichQuote;
+var whichBuzz;
+var lastBuzz;
+var whichCol
+
+if (Modernizr.touch){
+    $('html').addClass('touch');
+}
+
+$("#contactMobileButton").click(function() {
+    console.log('??')
+    $('html, body').animate({
+        scrollTop: $("#contactMobile").offset().top
+    }, 500);
+});
+
+randomColour();
+
+$(window).load(function(){
+    if (document.getElementById('buzzwordHere')){
+        getBuzzword();
+    }
+    getQuote();
+    setInterval(function(){
+        getQuote();
+    }, 10000);
+
+    if(document.getElementById('hodgePodge')){
+    var container = document.querySelector('#hodgePodge');
+    var msnry = new Masonry( container, {
+      itemSelector: '.projectTile'
+    });
+    }
+    $('#hodgePodge').removeClass('hidden');
+})
+
+
+
+$("#buzzwords").click(function(){
+    getBuzzword();
+})
+
+
+function randomColour(){
+    whichCol = Math.ceil(Math.random()*(colours.length))-1
+    $('html').addClass(colours[whichCol]);
+}
+
+
+function getQuote(){
+     $('#leftQuote').addClass('faded');
+     $('#rightQuote').addClass('faded');
+
+     whichQuote = Math.ceil(Math.random()*(leftQuote.length))-1
+
+     if (whichQuote == lastQuote) {
+        getQuote();
+     } else {
+        setTimeout(function(){
+            document.getElementById('leftQuote').innerHTML = leftQuote[whichQuote]
+            document.getElementById('rightQuote').innerHTML = rightQuote[whichQuote]
+            $('#leftQuote').removeClass('faded');
+            $('#rightQuote').removeClass('faded');
+            lastQuote = whichQuote;
+        }, 1000)
+     }
+
+}
+
+function getBuzzword(){
+     $('#buzzwordPara').addClass('faded');
+
+     whichBuzz = Math.ceil(Math.random()*(buzzWords.length))-1
+
+     if (whichBuzz == lastBuzz){
+        getBuzzword();
+     } else {
+        setTimeout(function(){
+            document.getElementById('buzzwordHere').innerHTML = buzzWords[whichBuzz]
+            $('#buzzwordPara').removeClass('faded');
+            lastBuzz = whichBuzz;
+        }, 500)
+        
+     }
+}
+
+
+if(document.getElementById('coverPic')){
+    var src = document.getElementById('coverPic').className;
+    var coverImage = $( '<img src="images/' + src + '.jpg">' );
+    coverImage.bind( 'load', function(){
+        setTimeout(function(){
+            $('.duck').removeClass('duck');
+            $('.coverImageLoader').addClass('hidden');
+        }, 200)
+        $('#coverPic').css( 'background-image', 'url(images/' + src + '.jpg)' );
+    });
+    if(coverImage[0].width ){
+        coverImage.trigger('load');
+    }
+}
+
